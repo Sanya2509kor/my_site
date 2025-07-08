cde3eeb180088100970828115eb7580e9ca89c0c
Aleksandr <Korchemkins0@yandex.ru>
2025-07-08 08:34:09 +0700

    update setings

--- app/settings.py
+++ app/settings.py
@@ -91,28 +91,28 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # на макбуке
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'home',
-#         'USER': 'home',
-#         'PASSWORD': 'home',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-# на пк
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'home',
-        'USER': 'aleksandr',
-        'PASSWORD': '89526136205Sa',
+        'USER': 'home',
+        'PASSWORD': 'home',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+# # на пк
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'home',
+#         'USER': 'aleksandr',
+#         'PASSWORD': '89526136205Sa',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
